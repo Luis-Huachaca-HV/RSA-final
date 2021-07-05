6b417c219c723ef26e4f9f07b1dcddc465e94817
Angel Loayza <angelloayzah2016@gmail.com>
2021-07-05 08:36:16 -0500

    lectura de datos por txt y mejora de interfaz y bugs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <NTL/ZZ.h>
 #include <iostream>
 #include "librerias.h"
+#include <cstdlib>
+#include <fstream>
 
+using std::ios;
+using std::ifstream;
 using namespace std;
 using namespace NTL;
 
@@ -10,7 +14,7 @@ class RSA
 private:
     ZZ d_R;
     ZZ d_E;
-    string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-( )abcdefghijklmnopqrstuvwxyz<>*1234567890#";
+    string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-( )abcdefghijklmnopqrstuvwxyz<>*1234567890[]#";
     //string alfabeto = "abcdefghijklmnopqrstuvwxyz#";
 
 public:
@@ -34,112 +38,76 @@ public:
 
     }
     void GenerateKeys()
-        {
-             //Generacion de claves
-            ZZ p_0 = ZZ(0), q_0 = ZZ(0);
-            GK_searchPQ(p_0,q_0);
-            //p_0 = 17;
-            //q_0 = 43;
+    {
+         //Generacion de claves
+        ZZ p_0 = ZZ(0), q_0 = ZZ(0);
+        GK_searchPQ(p_0,q_0);
+        //p_0 = 17;
+        //q_0 = 43;
 
-            //int n;
-            n_R = GK_searchN(p_0,q_0);
+        //int n;
+        n_R = GK_searchN(p_0,q_0);
 
-            ZZ fhi = GK_searchfhi(p_0,q_0);
+        ZZ fhi = GK_searchfhi(p_0,q_0);
 
-            //int e;
-            //e = 101;
-            e_R = GK_searchE(fhi);
+        //int e;
+        //e = 101;
+        e_R = GK_searchE(fhi);
 
-            //int d;
-            d_R = inversa(e_R,fhi);
+        //int d;
+        d_R = inversa(e_R,fhi);
 
-            cout << "p: " << p_0 << endl;
-            cout << "q: " << q_0 << endl;
-            cout << "fhi: " << fhi << endl;
-            cout << "---------------------------------------------------------------" << endl;
-            cout << "clave publica e(Receptor): " << e_R << endl;
-            cout << "clave privada d(Receptor):" << d_R << endl;
-            cout << "n(Receptor): " << n_R << endl;
-            cout << "---------------------------------------------------------------"  << endl;
-        }
-
-    void FirmaDigital()
-        {
-             //Generacion de claves
-            ZZ p_F = ZZ(0), q_F = ZZ(0);
-            GK_searchPQ(p_F,q_F);
-            //p_0 = 17;
-            //q_0 = 43;
-
-            //int n;
-            n_E = GK_searchN(p_F,q_F);
-
-            ZZ fhi_F = GK_searchfhi(p_F,q_F);
-
-            //int e;
-            //e = 101;
-            e_E = GK_searchE(fhi_F);
-
-            //int d;
-            d_E = inversa(e_E,fhi_F);
-
-            cout << "p: " << p_F << endl;
-            cout << "q: " << q_F << endl;
-            cout << "fhi: " << fhi_F << endl;
-            cout << "---------------------------------------------------------------" << endl;
-            cout << "clave publica e(Emisor): " << e_E << endl;
-            cout << "clave privada d(Emisor):" << d_E << endl;
-            cout << "n(Emisor): " << n_E << endl;
-            cout << "---------------------------------------------------------------"  << endl;
-        }
+        cout << "p: " << p_0 << endl;
+        cout << "q: " << q_0 << endl;
+        cout << "fhi: " << fhi << endl;
+        cout << "---------------------------------------------------------------" << endl;
+        cout << "clave publica e: " << e_R << endl;
+        cout << "clave privada d:" << d_R << endl;
+        cout << "n: " << n_R << endl;
+        cout << "---------------------------------------------------------------"  << endl;
+    }
 
     string EmisorEncryption()
     {
         cout << "--------------------- DATOS -----------------------------------" << endl;
-        cout << "Tu mensaje es: " << mensaje << endl;
-        cout << "clave publica e: " << e_R << endl;
-        cout << "n: " << n_R << endl;
+        cout << "Tu mensaje es: " << endl;
+        cout << mensaje << endl;
+        cout << "clave publica e: " << endl;
+        cout << e_R << endl;
+        cout << "n: " << endl;
+        cout << n_R << endl;
         cout << "---------------------------------------------------------------" << endl;
 
         mensaje_cifrado = cypher(mensaje_cifrado,e_R,n_R,mensaje,alfabeto);
 
-        cout << "----------------------Rubrica-----------------------------------" << endl;
-<<<<<<< HEAD
-        FirmaDigital();
+        cout << "---------------------- Rubrica -----------------------------------" << endl;
+        cout << "Ingrese la clave privada d del Emisor: " << endl;
+        cin >> d_E;
+        cout << "Ingrese la clave publica n del Emisor" << endl;
+        cin >> n_E;
         string rubrica_r = cypher_rubrica(d_E,n_E,alfabeto);
-        cout << "----------------------Firma Digital-----------------------------------" << endl;
 
+        cout << "---------------------- Firma Digital --------------------------" << endl;
+
+        cout << "Ingrese la clave publica e del Receptor: : " << endl;
+        cin >> e_R;
+        cout << "Ingrese la clave publica n del Receptor: : " << endl;
+        cin >> n_R;
         string firma_s = cypher_firma(rubrica_r,e_R,n_R,alfabeto);
 
-        cout << "El mensaje cifrado final es: " << mensaje_cifrado << endl;
-        cout << "La rubrica es: " << rubrica_r << endl;
-        cout << "Firma S: " << firma_s << endl;
+        cout << "---------------------- RESULTS --------------------------" << endl;
+        cout << "El mensaje cifrado final es: " << mensaje_cifrado << endl << endl;
+        cout << "La rubrica es: " << rubrica_r << endl << endl;
+        cout << "Firma S: " << firma_s << endl << endl;
 
         cout << "Fin del cifrado" << endl;
-=======
-        // Rubrica:  r = (mensaje ^ clave privada del emisor) modulo clave publica N_a
-        ZZ r, S;
-        r = k_ary(String_To_ZZ(mensaje_cifrado), d_E, n_E);
-        cout << "formula: (" << mensaje_cifrado << ")^" << d_E << " mod " << n_E << endl;
-        cout << "Rubrica: " << r << endl;
-        //cout << "Rubrica: " << r << endl;
-
-        //Firma Digital: S = rubrica ^ clave publica e del receptor modulo N_b
-        cout << "\n----------------------Firma Digital-----------------------------------" << endl;
-        S = k_ary(r, e_R, n_R);
-        cout << "formula: (" << r << ")^" << e_R << " mod " << n_R << endl;
-        cout << "Firma Digital: " << S << endl;
-        //cout << "Firma digital: " << S << endl;
-
-        cout << "El mensaje cifrado final es: " << mensaje_cifrado << endl;
->>>>>>> 984b0c426e76aacdf73f95425d8c7fe9c7e8bc34
 
         return mensaje;
     }
 
-    string ReceptorDecoded() //n= p * q
+    string ReceptorDecoded()
     {
-        ZZ p,q,S;
+        ZZ p,q;
         //p = 49152;
         //q = 59159;
 
@@ -152,73 +120,75 @@ public:
         //private_key;
         d_R = inversa(e_R,fhi);
 
-        cout << "Ingrese la firma S: " << endl; cin >> S;
-
         cout << "--------------------- DATOS -----------------------------------" << endl;
-        cout << "Tu mensaje es: " << mensaje << endl;
-        cout << "p: " << p << endl;
-        cout << "q: " << q << endl;
-        cout << "fhi: " << fhi << endl;
-        cout << "private_key: " << d_R << endl;
-        cout << "n: " << n_R << endl;
+        cout << "Tu mensaje es: " << endl;
+        cout << mensaje << endl;
+        cout << "p: " << endl;
+        cout << p << endl;
+        cout << "q: " << endl;
+        cout << q << endl;
+        cout << "fhi: " << endl;
+        cout << fhi << endl;
+        cout << "private_key: " << endl;
+        cout << d_R << endl;
+        cout << "n: " << endl;
+        cout << n_R << endl;
         cout << "---------------------------------------------------------------" << endl;
 
-        ZZ i_fin_temp = NumberOfDigitsZZ(n_R); //4
-        ZZ i_fin_temp2 = ZZ(mensaje.length()) / i_fin_temp; //6 bloques
+        mensaje_decifrado = decypher(mensaje,mensaje_decifrado,n_R,d_R,alfabeto);
 
-        string mensaje_decifrado_num;
-        for (int i = 0; i < i_fin_temp2; ++i) //es 6 = mensaje_num.length() / k
-        {
-            //string s_num_matriz = SeparateIntoBlocks(i, ZZ_to_int(NumberOfDigitsZZ(n)) ,mensaje); //0234
-            string s_num_matriz = SeparateIntoBlocks(i, ZZ_to_int(i_fin_temp) ,mensaje); //0234
+        string firma_digital;
+        cout << "Ingrese la firma digital: " << endl;
+        cin >> firma_digital;
+        string firma_to_rubrica = decypher_firma(d_R,n_R,firma_digital);
 
-            ZZ i_num_matriz = String_To_ZZ(s_num_matriz); //234
+        cout << "Ingrese clave publica e (emisor): "<< endl;
+        cin >> e_E;
+        cout << "Ingrese clave publica n (emisor): "<< endl;
+        cin >> n_E;
 
-            cout << "formula: (" << i_num_matriz << ")^" << d_R << " mod " << n_R << endl;
+        //string mensaje_decifrado_rubrica;
+        string firma_descifrada;
+        firma_descifrada = decypher_rubrica(firma_descifrada,firma_to_rubrica,e_E,n_E,alfabeto);
+        //cout << "mensaje descifrado normal: " << mensaje_decifrado << endl;
+        //cout << "mensaje descifrado firma digital: " << mensaje_decifrado_rubrica << endl;
 
-            ZZ i_aux;
-            //i_aux = ModularArithmetic(i_num_matriz,d,n); //21
-            //i_aux = binpow(i_num_matriz,d,n); //21
-            //i_aux = powerZZ(i_num_matriz,d,n); //21
-            i_aux = k_ary(i_num_matriz,d_R,n_R); //21
-            //int i_aux2 = ZZ_to_int(i_aux);
-
-            ZZ total_number_digits = NumberOfDigitsZZ(n_R)-1; //3
-            mensaje_decifrado_num = IncreaseZeros(i_aux,total_number_digits,mensaje_decifrado_num); //021412...
-            cout << "Mensaje descifrado incompleto: " << mensaje_decifrado_num << endl;
-        }
-
-        cout << "El mensaje descifrado en numeros es: " << mensaje_decifrado_num << endl;
-
-        //Verificación del mensaje mandado con firma digital
-        ZZ D;
-        cout << "\n----------------------Verificacion-----------------------------------" << endl;
-        D = k_ary(S, d_R, n_R);
-        cout << "formula: (" << mensaje_decifrado_num << ")^" << d_R << " mod " << n_R << endl;
-        cout << D << endl;
-        cout << "True" << endl;
-        cout << "Emisor Verified" << endl;
-
-        ZZ i_fin_temp3 = ZZ(NumberOfDigits(alfabeto.length())); //2
-        ZZ i_fin_temp4 = ZZ(mensaje_decifrado_num.length()) / i_fin_temp3; // 18/2 = 9 bloques
-        cout <<" i fin temp4 " <<i_fin_temp4 << endl;
-        for (int i = 0; i < i_fin_temp4; ++i) //es 9 = mensaje_num.length() / 2
-        {
-            string s_num_matriz = SeparateIntoBlocks(i, NumberOfDigits(alfabeto.length()),mensaje_decifrado_num); //02
-            cout << s_num_matriz << endl;
-
-            int i_num_matriz = atoi(s_num_matriz.c_str());
-            string aux = ObtainMessageInLetters(i_num_matriz,alfabeto);
-            mensaje_decifrado.append(aux);
-
-        }
-
-        cout << "\n----------------------Mensaje Descifrado-----------------------------------" << endl;
-        cout << "El mensaje descifrado es: " << mensaje_decifrado << endl;
-
-        return mensaje;
+        cout << "---------------------- RESULTS --------------------------" << endl;
+        cout << "El mensaje descifrado es: " << mensaje_decifrado << endl << endl;
+        cout << "La rubrica es: " << firma_to_rubrica << endl << endl;
+        cout << "La firma descifrada es: " << firma_descifrada << endl << endl;
     }
 };
+
+string leerMensajeletras(){
+    ifstream archivo;
+    string texto, resultado;
+    archivo.open("mensaje_largo.txt",ios::in);
+    if(archivo.fail()){
+        cout<<"Error al abrir el archivo de texto cifrado\n";
+        exit(1);
+    }
+    while(!archivo.eof()){
+        getline(archivo,texto);
+        resultado += texto;
+    }
+    return resultado;
+}
+
+string leerMensajenumeros(){
+    ifstream archivo;
+    string texto, resultado;
+    archivo.open("mensaje_numeros.txt",ios::in);
+    if(archivo.fail()){
+        cout<<"Error al abrir el archivo de texto cifrado\n";
+        exit(1);
+    }
+    while(!archivo.eof()){
+        getline(archivo,texto);
+        resultado += texto;
+    }
+    return resultado;
+}
 
 int main()
 {
@@ -236,30 +206,28 @@ int main()
 
     if(respuesta == 1) //emisor
     {
-        cout << "Ingrese el mensaje que desea cifrar: ";
-        cin.ignore();
-        //cin >> mensaje;
-        getline(cin,mensaje);
-        cout << "-------------------------------------------------------------" << endl;
-        cout << "Ingrese la clave publica e: " << endl;
+        cout << " ------------------ BIENVENIDO EMISOR :) --------------------------- " << endl;
+        mensaje = leerMensajeletras();
+        cout << "Mensaje leido del .txt correctamente" << endl;
+        cout << "--------------------- Cifrado del mensaje -------------------------------" << endl;
+        cout << "Ingrese la clave publica e del receptor : " << endl;
         cin >> e;
-        cout << "Ingrese la clave publica n: " << endl;
+        cout << "Ingrese la clave publica n del receptor : " << endl;
         cin >> n;
-
         RSA emisor(n,e,mensaje);
         emisor.EmisorEncryption();
         cout << "-------------------------------------------------------------" << endl;
     }
     else if (respuesta == 2) //receptor
     {
-        cout << "Ingrese el mensaje que desea descifrar: ";
-        cin.ignore();
-        //cin >> mensaje;
-        getline(cin,mensaje);
+        cout << " ------------------ BIENVENIDO RECEPTOR :) --------------------------- " << endl;
+        cout << "------------------ Descifrado del mensaje --------------------" << endl;
+        mensaje = leerMensajenumeros();
+        cout << "Mensaje leido del .txt correctamente" << endl;
         cout << "-------------------------------------------------------------" << endl;
-        cout << "Ingrese la clave publica e: " << endl;
+        cout << "Ingrese la clave publica e del receptor: " << endl;
         cin >> e;
-        cout << "Ingrese la clave publica n: " << endl;
+        cout << "Ingrese la clave publica n del receptor: " << endl;
         cin >> n;
 
         RSA receptor(n,e,mensaje);
